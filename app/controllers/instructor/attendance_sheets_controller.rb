47324df2333ead88f4663364aa7049669cbc9ded
Spencer Eldred <eldredspencer@gmail.com>
2014-07-14 16:12:32 -0600

    Add ability to take attendance for past dates

--- app/controllers/instructor/attendance_sheets_controller.rb
+++ app/controllers/instructor/attendance_sheets_controller.rb
@@ -3,17 +3,31 @@ class Instructor::AttendanceSheetsController < InstructorRequiredController
     @attendance_sheet = AttendanceSheet.new
     @cohort = user_session.current_user.cohort
     @students = @cohort.students
+    # set holidays to National Holidays only
+    @holidays = [Date.new(2014,7,4), Date.new(2014,9,1)]
+    @today = Date.today
+    # set submitted dates to be per current_user's cohort
+    @submitted_dates = AttendanceSheet.where(cohort_id: @cohort[:id]).map {|sheet| sheet.sheet_date }
+    @class_dates = (date_range(@cohort, @today) - @submitted_dates - @holidays).reverse!
   end
 
   def create
     cohort = user_session.current_user.cohort
 
     AttendanceSheet.create!(
-      sheet_date: Date.today,
+      sheet_date: params[:dropdown_date],
       cohort_id: cohort.id,
       attendances_attributes: params[:attendance_sheet][:attendances]
     )
 
     redirect_to instructor_cohort_path(cohort), notice: "Attendance successfully submitted"
   end
+
+  def date_range(cohort, today)
+    start_date = cohort.start_date
+    @days_in_class = (start_date..today).map {|day| day unless day.saturday? or day.sunday? }
+    @days_in_class.compact!
+  end
+
+
 end
--- spec/features/instructors/attendance_spec.rb
+++ spec/features/instructors/attendance_spec.rb
@@ -2,19 +2,22 @@ require "spec_helper"
 
 feature "Attendance" do
 
-  let!(:cohort) { create_cohort(name: "Boulder gSchool") }
-  let!(:instructor) { create_user(first_name: "Instructor", last_name: "User", github_id: "987", role_bit_mask: 1, cohort_id: cohort.id) }
-  let!(:student) { create_user(first_name: "Student", last_name: "User", github_id: "123", cohort_id: cohort.id, github_username: "Student12345") }
-  let!(:other_student) { create_user(first_name: "Other", last_name: "Student", github_id: "321", cohort_id: cohort.id, github_username: "Student54321") }
+  let!(:boulder_cohort) { create_cohort(name: "Boulder gSchool") }
+  let!(:denver_cohort) { create_cohort(name: "Denver gSchool") }
+  let!(:instructor) { create_user(first_name: "Instructor", last_name: "User", github_id: "987", role_bit_mask: 1, cohort_id: boulder_cohort.id) }
+  let!(:student) { create_user(first_name: "Student", last_name: "User", github_id: "123", cohort_id: boulder_cohort.id, github_username: "Student12345") }
+  let!(:other_student) { create_user(first_name: "Other", last_name: "Student", github_id: "321", cohort_id: boulder_cohort.id, github_username: "Student54321") }
+  let!(:other_cohort_student) { create_user(first_name: "Other", last_name: "Cohort", github_id: "789", cohort_id: denver_cohort.id, github_username: "Student23456") }
 
   before do
+    @today = Date.new(2014, 7, 11) # set @today to known Friday
     sign_in(instructor)
   end
 
-  scenario "As an instructor, I can take attendance" do
+  scenario "As an instructor, the first date is today if not entered already" do
     click_on "Attendance"
 
-    expect(page).to have_content "Attendance for #{Date.today.to_s}"
+    expect(page).to have_content "#{@today.to_s}"
 
     check "Student User"
     uncheck "Other Student"
@@ -23,4 +26,77 @@ feature "Attendance" do
 
     expect(page).to have_content "Attendance successfully submitted"
   end
+
+  scenario "As an instructor, I can enter attendance for my students" do
+    click_on "Attendance"
+
+    expect(page).to have_content "Student User"
+    expect(page).to have_content "Other Student"
+  end
+
+  scenario "As an instructor, I should not be able to enter attendance on students from other cohorts" do
+    click_on "Attendance"
+
+    expect(page).not_to have_content "Other Cohort"
+  end
+
+  scenario "As an instructor, I expect to see my cohort name" do
+    click_on "Attendance"
+
+    expect(page).to have_content "#{boulder_cohort.name}"
+  end
+
+  scenario "As an instructor, I should not be able to enter today's attendance twice" do
+    skip
+    click_on "Attendance"
+    expect(page).to have_content "#{@today.to_s}"
+    click_on "Submit Attendance" # submits @today
+    expect(page).to have_content "Attendance successfully submitted"
+
+    click_on "Attendance"
+
+    expect(page).not_to have_content "#{@today.to_s}"
+
+    click_on "Submit Attendance"
+
+    expect(page).to have_content "Attendance successfully submitted"
+
+  end
+
+  scenario "As an instructor, I can take select attendance for a previous date" do
+    click_on "Attendance"
+
+    select "#{@today.yesterday.to_s}"
+
+    click_on "Submit Attendance"
+
+    expect(page).to have_content "Attendance successfully submitted"
+
+  end
+
+  scenario "As an instructor, I should not be able to enter attendance on Saturday" do
+    @today = Date.new(2014,7,12) # set @today to a known Saturday
+    click_on "Attendance"
+
+    expect(page).not_to have_content "#{@today}"
+
+  end
+
+  scenario "As an instructor, I should not be able to enter attendance on Sunday" do
+    @today = Date.new(2014,7,13) # set @today to a known Sunday
+    click_on "Attendance"
+
+    expect(page).not_to have_content "#{@today}"
+
+  end
+
+  scenario "As an instructor, I should not be able to enter attendance on a Holiday" do
+    @today = Date.new(2014,7,4) # set @today to known holiday
+    click_on "Attendance"
+
+    expect(page).not_to have_content "#{@today}"
+
+  end
+
+
 end
