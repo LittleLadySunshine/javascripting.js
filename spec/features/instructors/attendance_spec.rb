e2c190e6dfb248af4ac0b61dc9af84b605a55167
Spencer Eldred <eldredspencer@gmail.com>
2014-07-18 10:03:23 -0600

    Modified test based on pull request feedback

--- spec/features/instructors/attendance_spec.rb
+++ spec/features/instructors/attendance_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "rails_helper"
 
 feature "Attendance" do
 
@@ -14,7 +14,7 @@ feature "Attendance" do
     sign_in(instructor)
   end
 
-  scenario "As an instructor, the first date is today if not entered already" do
+  scenario "As an instructor, I can take attendance" do
     click_on "Attendance"
 
     expect(page).to have_content "#{@today.to_s}"
@@ -27,16 +27,11 @@ feature "Attendance" do
     expect(page).to have_content "Attendance successfully submitted"
   end
 
-  scenario "As an instructor, I can enter attendance for my students" do
+  scenario "As an instructor, I should not be able to enter attendance on students from other cohorts" do
     click_on "Attendance"
 
     expect(page).to have_content "Student User"
     expect(page).to have_content "Other Student"
-  end
-
-  scenario "As an instructor, I should not be able to enter attendance on students from other cohorts" do
-    click_on "Attendance"
-
     expect(page).not_to have_content "Other Cohort"
   end
 
