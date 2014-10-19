69cda174a9432ad021b5a0f65e10e404c5d5bd6e
Jeff Dean <jeff@galvanize.it>
2014-10-19 14:46:56 -0600

    updated some formatting on the students pages

--- app/controllers/student/class_notes_controller.rb
+++ app/controllers/student/class_notes_controller.rb
@@ -7,10 +7,11 @@ class Student::ClassNotesController < SignInRequiredController
   def index
     client = Octokit::Client.new(access_token: session[:access_token])
 
+    @date = Date.today
     begin
       @content = client.contents(
         'gSchool/boulder-g4-assets',
-        path: "class-notes/class-notes-#{Date.today.strftime("%Y-%m-%d")}.md",
+        path: "class-notes/class-notes-#{@date.strftime("%Y-%m-%d")}.md",
         accept: "application/vnd.github.VERSION.html"
       )
     rescue Octokit::NotFound => e
