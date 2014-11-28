b817c42a5258cb9eae0d43f7877f980b79241bd7
Jeff Dean <jeff@galvanize.it>
2014-11-28 14:20:43 -0700

    fixed bug in daily plans grouping, and bug in class project features edit breadcrumbs

--- app/models/daily_plan.rb
+++ app/models/daily_plan.rb
@@ -3,7 +3,7 @@ class DailyPlan < ActiveRecord::Base
   belongs_to :cohort
 
   validates :cohort, presence: true
-  validates :date, presence: true
+  validates :date, presence: true, uniqueness: {scope: :cohort_id}
   validates :description, presence: true
 
   def self.ordered
@@ -12,10 +12,10 @@ class DailyPlan < ActiveRecord::Base
 
   def self.grouped_by_week(cohort)
     grouped_plans = where(cohort_id: cohort).group_by do |plan|
-      plan.date.strftime("%U")
+      [plan.date.strftime("%G").to_i, plan.date.strftime("%V").to_i]
     end
 
-    grouped_plans.map.with_index(1) do |(_, days), week_number|
+    grouped_plans.sort_by(&:first).map.with_index(1) do |(_, days), week_number|
       [week_number, days.sort_by(&:date)]
     end.reverse
   end
