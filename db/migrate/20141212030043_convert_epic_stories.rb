d6ffcf32f3bea943b2aa33e027ebd56fe6df0412
Jeff Dean <jeff@galvanize.it>
2014-12-11 21:00:25 -0700

    gave up on the migration

--- db/migrate/20141212030043_convert_epic_stories.rb
+++ db/migrate/20141212030043_convert_epic_stories.rb
@@ -1,36 +1,5 @@
 class ConvertEpicStories < ActiveRecord::Migration
-
-  class MigrationEpic < ActiveRecord::Base
-    self.table_name = :epics
-  end
-
-  class MigrationStory < ActiveRecord::Base
-    self.table_name = :stories
-  end
-
-  require 'csv'
-
   def up
     change_column :stories, :description, :text
-    MigrationEpic.reset_column_information
-    MigrationStory.reset_column_information
-    MigrationStory.delete_all
-
-    MigrationEpic.all.each do |epic|
-      if epic.stories?
-        stories = CSV.parse(epic.stories, headers: true)
-        labels = []
-        stories.each_with_index do |story, index|
-          MigrationStory.create!(
-            epic_id: epic.id,
-            title: story['Title'] || story['Story'],
-            description: story['Description'],
-            position: index,
-          )
-          labels += story['Labels'].split(",").map(&:strip).reject{|label| %w(mvp stretch).include?(label) }
-        end
-        epic.update(label: labels.first)
-      end
-    end
   end
 end
